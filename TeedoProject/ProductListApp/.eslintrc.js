900ee0ab8bf4c45f37a6f76eae38bc2f91e52157
Denizhan <denizhan.misirlioglu@gmail.com>
2025-02-03 20:46:03 +0300

    chore: Setup ESLint and Prettier configuration

---
+++ TeedoProject/ProductListApp/.eslintrc.js
@@ -0,0 +1,33 @@
+// .eslintrc.js
+module.exports = {
+    parser: '@typescript-eslint/parser', // TypeScript için parser
+    parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    env: {
+      browser: true,
+      node: true,
+      es6: true,
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:@typescript-eslint/recommended', // TypeScript önerileri
+      'prettier',
+    ],
+    plugins: ['react', '@typescript-eslint', 'prettier'],
+    rules: {
+      'prettier/prettier': 'error', // Prettier kurallarını ESLint hatası olarak göster
+      // İsteğe bağlı: diğer özel kurallarınızı buraya ekleyebilirsiniz
+    },
+  };
+  
